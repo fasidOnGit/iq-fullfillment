68726289956d2e36b6a230bfc5585ccd0228d628
Mohamed Riyas <riyas90cse@gmail.com>
2020-06-12 00:02:21 +0300

    Login Form UI Component Added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,17 @@
+// @ts-ignore
 import { BrowserModule } from '@angular/platform-browser';
+// @ts-ignore
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+// @ts-ignore
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
